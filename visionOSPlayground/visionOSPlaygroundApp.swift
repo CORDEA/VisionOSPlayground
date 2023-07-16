2c72cfaedd182875a9ad2dc8d94ffe5d2c3591c2
Yoshihiro Tanaka <contact@cordea.jp>
2023-07-16 17:13:25 +0900

    add a project

---
+++ visionOSPlayground/visionOSPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionOSPlaygroundApp.swift
+//  visionOSPlayground
+//
+//  Created by Yoshihiro Tanaka on 2023/07/16.
+//
+
+import SwiftUI
+
+@main
+struct visionOSPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
